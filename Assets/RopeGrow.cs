5c315d8213a600d71aea1e94cb9c865ba767785f
Seanna M <evilseanbot@gmail.com>
2019-05-26 14:27:41 -0700

    first commit

---
+++ Assets/RopeGrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeGrow : MonoBehaviour
+{
+    public Obi.ObiDistanceConstraints distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance.stretchingScale += 0.01f;
+        transform.localScale = new Vector3(transform.localScale.x + 0.1f, transform.localScale.y, transform.localScale.z);
+    }
+}
