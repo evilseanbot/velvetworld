1aa42914bb9667d9834a0584d281c7a7c18c58b5
Seanna <evilseanbot@gmail.com>
2019-06-14 01:20:12 -0700

    Made environment interesting

---
+++ Assets/RecenterPlayspace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecenterPlayspace : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject pillow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.Euler(0, pillow.transform.eulerAngles.y - (head.transform.localRotation.eulerAngles.y), 0);
+
+            transform.position = (pillow.transform.position - (transform.rotation * head.transform.localPosition));
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            //transform.rotation = (pillow.transform.rotation *  (Quaternion.Inverse(head.transform.rotation)));
+        }
+    }
+}
---
+++ Assets/Rope/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float offset;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(Mathf.Sin(Time.time+offset)/2f, 0, Mathf.Cos(Time.time+offset)/2f);
+    }
+}
